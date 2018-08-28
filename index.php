596525d43abdaee2ff5c72fc60686d21d617be56
Joshua Britt <joshua@britt.email>
2018-08-28 05:20:41 -0500

    Renamed description to reflect broader purpose.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Arcane: Intuitive PHP Prototyping
+ * Arcane: Intuitive Web Prototyping
  * Copyright 2017-2018 Joshua Britt
  * https://github.com/capachow/arcane/
  * Released under the MIT License
