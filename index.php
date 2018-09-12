884d28f09a2c49b24d5dd8944a8d181c1f0bcb05
Joshua Britt <joshua@britt.email>
2018-09-11 23:27:27 -0500

    Automated cache serializing for arrays and objects.

--- index.php
+++ index.php
@@ -45,14 +45,24 @@ function cache($path, $content = null) {
     } else if(is_null($content)) {
       if(file_exists($file)) {
         $content = file_get_contents($file);
+
+        if(preg_match("/^[aO]:.+}$/", $content)) {
+          $content = unserialize($content);
+        }
       }
 
       return $content;
     } else {
       array_map('unlink', glob(strtok($file, '.') . '.*.php'));
 
+      if(is_array($content) || is_object($content)) {
+        $content = serialize($content);
+      }
+
       file_put_contents($file, $content);
     }
+  } else {
+    return false;
   }
 }
 
@@ -162,9 +172,7 @@ function scribe($string) {
 (function() {
   $locales = cache(DIR['LOCALES']) ?? [];
 
-  if(!empty($locales)) {
-    $locales = unserialize($locales);
-  } else {
+  if(empty($locales)) {
     $directory = rtrim(path(DIR['LOCALES'], true), '/');
 
     foreach(glob($directory . '/*/*[-+]*.json') as $locale) {
@@ -214,7 +222,7 @@ function scribe($string) {
     }
 
     if(!empty($locales)) {
-      cache(DIR['LOCALES'], serialize($locales));
+      cache(DIR['LOCALES'], $locales);
     }
   }
 
