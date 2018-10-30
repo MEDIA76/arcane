651605c7265406531fbc79e06ab8620383336375
Joshua Britt <joshua@britt.email>
2018-10-29 21:44:27 -0500

    Fixed fatal bug while looping through helper directories.

--- index.php
+++ index.php
@@ -273,7 +273,7 @@ function scribe($string) {
   do {
     $directories[] = $path;
     $path = dirname($path);
-  } while($path != __ROOT__);
+  } while($path != APP['DIR']);
 
   foreach(array_reverse($directories) as $directory) {
     if(is_dir($directory)) {
