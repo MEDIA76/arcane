17565e9b300c083cd47af770f84bc6c4d8a16248
Joshua Britt <joshua.britt@pm-international.com>
2019-07-16 17:49:45 -0500

    Updated project version number.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Arcane 19.06.2 Microframework
+ * Arcane 19.07.1 Microframework
  * Copyright 2017-2019 Joshua Britt
  * https://github.com/MEDIA76/arcane
  * Released under the MIT License
